48e5eaf50fedb7efe091d617024783c9134875de
xstupi00 <xstupi00@stud.fit.vutbr.cz>
2020-11-25 10:37:59 +0100

    Refactor collecting of indicators

--- perun/utils/indicators.py
+++ perun/utils/indicators.py
@@ -19,6 +19,8 @@ BLOCK_REGEX = r"^\s*(%{5}|\d+):\s+\d+\-block\s+\d+$"
 CALL_REGEX = r"(call)\s+\d+\s+(never\s+executed|returned\s+\d+(?:\.\d+)?%)$"
 BRANCH_REGEX = r"^(branch)\s+\d+\s+(never|taken)\s+(executed|\d+|\d+(?:\.\d+)?%)(\s+\(fallthrough\)){0,}$"
 FUNCTION_END_REGEX = r"^\s+(#{5}|-|\d+):\s+\d+:}$"
+NM_REGEX = r"/_GLOBAL__sub_(D|I)_00100_(0|1)_[A-Za-z_]/"
+STAP_SCRIPT_NAME = "func_info.stp"
 
 
 def collect(object_path, ignore_list):
@@ -29,14 +31,54 @@ def collect(object_path, ignore_list):
             executable = jobs[0].executable
             object_path = os.path.abspath(object_path or os.path.abspath(executable.cmd))
             fuzz_utils.prepare_workspace(object_path)
-            runner.run_prephase_commands("pre_run")
-            return_code = utils.run_external_command(shlex.split(executable.to_escaped_string()))
-            if return_code != 0:
-                raise CalledProcessError(return_code, executable.to_escaped_string())
-            stats.add_stats('indicators', ['gcov'], [get_gcov_indicators(object_path, set(ignore_list))])
+            runner.run_prephase_commands('pre_run')
+            collect_gcov_indicators(executable, object_path, ignore_list)
+            collect_stap_indicators(executable)
+            collect_nm_indicators(object_path)
 
 
-def get_gcov_indicators(object_path, ignore_list):
+def collect_nm_indicators(object_path):
+    nm_result = {}
+    awk_command = "awk '{ if(($2 == \"T\" || $2 == \"t\") && ($3 !~ " + NM_REGEX + ")) { print $3 }}'"
+    for object_file in glob.iglob(object_path + '/*.o'):
+        nm_command = "nm " + object_file + " | " + awk_command + " | wc -l"
+        nm_output, _ = utils.run_safely_external_command(nm_command)
+        nm_result[object_file.split("/")[-1]] = int(nm_output.decode('utf-8'))
+    stats.update_stats('indicators', ['nm'], [nm_result])
+
+
+def collect_stap_indicators(executable):
+    stap_script_name = os.path.join(os.path.split(__file__)[0], STAP_SCRIPT_NAME)
+    stap_command = \
+        'sudo stap ' + stap_script_name + " " + executable.cmd + ' -c ' + '"' + executable.to_escaped_string() + '"'
+    stap_output, _ = utils.run_safely_external_command(stap_command)
+    stats.update_stats('indicators', ['stap'], [_parse_stap_output(stap_output)])
+
+
+def _parse_stap_output(stap_output):
+    stap_result = {}
+    stap_output_list = stap_output.decode('utf-8').split('\n')
+    del stap_output_list[-1]
+    for function_info in stap_output_list:
+        info_list = function_info.split()
+        stap_result[info_list[0]] = {
+            "called": int(info_list[1]),
+            "insns": int(info_list[2]),
+            "cycles": int(info_list[3]),
+            "branches": int(info_list[4]),
+            "cacherefs": int(info_list[5])
+        }
+    return stap_result
+
+
+def collect_gcov_indicators(executable, object_path, ignore_list):
+    return_code = utils.run_external_command(shlex.split(executable.to_escaped_string()))
+    if return_code != 0:
+        raise CalledProcessError(return_code, executable.to_escaped_string())
+    stats.add_stats('indicators', ['gcov'], [_get_gcov_indicators(object_path, set(ignore_list))])
+
+
+def _get_gcov_indicators(object_path, ignore_list):
     def run_gcov():
         gcov_output = utils.get_stdout_from_external_command(gcov_command)
         return gcov_output
@@ -45,18 +87,20 @@ def get_gcov_indicators(object_path, ignore_list):
     objects_names = [obj for obj in objects_names if obj not in ignore_list]
     if fuzz_utils.get_gcov_version() >= fuzz_utils.GCOV_VERSION_W_INTERMEDIATE_FORMAT:
         gcov_command = ["gcov", "-i", "-m", "-t"] + objects_names
-        gcov_indicators = parse_gcov_output(json.loads(json.dumps(run_gcov().splitlines())))
+        gcov_indicators = _parse_gcov_output(json.loads(json.dumps(run_gcov().splitlines())))
     else:
         gcov_command = ["gcov", "-a", "-m", "-t"] + objects_names
-        gcov_indicators = parse_raw_gcov_output(run_gcov().splitlines())
+        gcov_indicators = _parse_raw_gcov_output(run_gcov().splitlines())
 
     return gcov_indicators
 
 
-def parse_gcov_output(output):
+def _parse_gcov_output(output):
     result = {}
     for module in output:
-        for file in json.loads(module)["files"]:
+        module = module[:module.rfind('}') + 1]
+        load_files = json.loads(module)["files"] if module.startswith('{') else []
+        for file in load_files:
             result[file["file"]] = {}
             for function_info in file["functions"]:
                 result[file["file"]][function_info["demangled_name"]] = {
@@ -68,7 +112,7 @@ def parse_gcov_output(output):
     return result
 
 
-def parse_raw_gcov_output(gcov_output):
+def _parse_raw_gcov_output(gcov_output):
     result = {}
     file_name, demangled_name = "", ""
     blocks_executed_perc, lines, execution_count = 0, 0, 0
@@ -109,71 +153,60 @@ def parse_raw_gcov_output(gcov_output):
     return result
 
 
-def _get_del_functions(head_gcov_stats, prev_gcov_stats):
-    del_functions = {}
+def _get_functions_diff(head_gcov_stats, prev_gcov_stats):
+    del_functions, new_functions = {}, {}
     for module in head_gcov_stats:
-        diff_min_value = min(0, len(head_gcov_stats[module]) - len(prev_gcov_stats.get(module, [])))
-        if diff_min_value < 0:
-            del_functions[module] = diff_min_value
-    return del_functions
+        new_functions[module] = len({*head_gcov_stats[module]} - {*prev_gcov_stats.get(module, {})})
+        del_functions[module] = len({*prev_gcov_stats.get(module, {})} - {*head_gcov_stats[module]})
+
+    del_functions = {k: v for (k, v) in del_functions.items() if v != 0}
+    new_functions = {k: v for (k, v) in new_functions.items() if v != 0}
+    return del_functions, new_functions
 
 
-def _get_new_functions(head_gcov_stats, prev_gcov_stats):
-    new_functions = {}
+def _get_diffs(head_gcov_stats, prev_gcov_stats, key):
+    diffs = {}
     for module in head_gcov_stats:
-        diff_max_value = max(0, len(head_gcov_stats[module]) - len(prev_gcov_stats.get(module, [])))
-        if diff_max_value > 0:
-            new_functions[module] = diff_max_value
-    return new_functions
-
-
-def _get_del_blocks(head_gcov_stats, prev_gcov_stats, block_key):
-    del_blocks = {}
-    for module in head_gcov_stats:
-        del_blocks[module] = {}
+        diffs[module] = {}
         for function in head_gcov_stats[module]:
-            head_blocks = head_gcov_stats[module][function][block_key]
-            prev_blocks = prev_gcov_stats.get(module, {}).get(function, {}).get(block_key, 0)
-            diff_min_blocks = min(0, head_blocks - prev_blocks)
-            if diff_min_blocks < 0:
-                del_blocks[module][function] = diff_min_blocks
-    return del_blocks
+            head_blocks = head_gcov_stats[module][function][key]
+            prev_blocks = prev_gcov_stats.get(module, {}).get(function, {}).get(key, 0)
+            if head_blocks - prev_blocks != 0:
+                diffs[module][function] = head_blocks - prev_blocks
 
-
-def _get_new_blocks(head_gcov_stats, prev_gcov_stats, block_key):
-    new_blocks = {}
-    for module in head_gcov_stats:
-        new_blocks[module] = {}
-        for function in head_gcov_stats[module]:
-            head_blocks = head_gcov_stats[module][function][block_key]
-            prev_blocks = prev_gcov_stats.get(module, {}).get(function, {}).get(block_key, 0)
-            diff_max_blocks = max(0, head_blocks - prev_blocks)
-            if diff_max_blocks > 0:
-                new_blocks[module][function] = diff_max_blocks
-    return new_blocks
-
-
-def _get_diff_values_from(head_gcov_stats, prev_gcov_stats, key):
-    diff_values = {}
-    for module in head_gcov_stats:
-        diff_values[module] = {}
-        for function in head_gcov_stats[module]:
-            head_value = head_gcov_stats[module][function][key]
-            prev_value = prev_gcov_stats.get(module, {}).get(function, {}).get(key, 0)
-            diff_value = head_value - prev_value
-            if diff_value != 0:
-                diff_values[module][function] = diff_value
-    return diff_values
+    diffs = {k: v for (k, v) in diffs.items() if v != {}}
+    return diffs
 
 
 def evaluate():
+    minor_version = 'edf3f97ae2af024708ebb4ac614227327033ca47'
+    evaluate_gcov_stats(minor_version)
+    evaluate_stap_stats(minor_version)
+
+
+def evaluate_gcov_stats(minor_version):
     head_gcov_stats = stats.get_stats_of('indicators')['gcov']
-    minor_version = '4f391796b7de78a434a2cc7107034603df414905'
     prev_gcov_stats = stats.get_stats_of('indicators', minor_version=minor_version)['gcov']
-    del_functions = _get_del_functions(head_gcov_stats, prev_gcov_stats)
-    new_functions = _get_new_functions(head_gcov_stats, prev_gcov_stats)
-    del_blocks = _get_del_blocks(head_gcov_stats, prev_gcov_stats, 'blocks')
-    del_exec_blocks = _get_del_blocks(head_gcov_stats, prev_gcov_stats, 'exec_blocks')
-    new_blocks = _get_new_blocks(head_gcov_stats, prev_gcov_stats, 'blocks')
-    new_exec_blocks = _get_new_blocks(head_gcov_stats, prev_gcov_stats, 'exec_blocks')
-    diff_loc = _get_diff_values_from(head_gcov_stats, prev_gcov_stats, 'lines')
+    del_functions, new_functions = _get_functions_diff(head_gcov_stats, prev_gcov_stats)
+    blocks_diff = _get_diffs(head_gcov_stats, prev_gcov_stats, 'blocks')
+    exec_blocks_diff = _get_diffs(head_gcov_stats, prev_gcov_stats, 'exec_blocks')
+    diff_loc = _get_diffs(head_gcov_stats, prev_gcov_stats, 'lines')
+
+
+def _get_diff_stats(head_stats, prev_stats):
+    diff_stats = {}
+    for function in head_stats:
+        diff_stats[function] = {
+            "called": head_stats[function]["called"] - prev_stats.get(function, {}).get("called", 0),
+            "insns": head_stats[function]["insns"] - prev_stats.get(function, {}).get("insns", 0),
+            "cycles": head_stats[function]["cycles"] - prev_stats.get(function, {}).get("cycles", 0),
+            "branches": head_stats[function]["branches"] - prev_stats.get(function, {}).get("branches", 0),
+            "cacherefs": head_stats[function]["cacherefs"] - prev_stats.get(function, {}).get("cacherefs", 0),
+        }
+    return diff_stats
+
+
+def evaluate_stap_stats(minor_version):
+    head_stap_stats = stats.get_stats_of('indicators')['stap']
+    prev_gcov_stats = stats.get_stats_of('indicators', minor_version=minor_version)['stap']
+    diff_stats = _get_diff_stats(head_stap_stats, prev_gcov_stats)
